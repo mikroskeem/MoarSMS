4ff35d16d53a3e1e9fef3a432802327f9dc388d4
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2018-02-23 22:04:03 +0200

    Give up and use Apache DigestUtils
    My implementation was broken whoops

--- src/main/kotlin/eu/mikroskeem/moarsms/Hash.kt
+++ src/main/kotlin/eu/mikroskeem/moarsms/Hash.kt
@@ -25,25 +25,14 @@
 
 package eu.mikroskeem.moarsms
 
-import java.security.MessageDigest
-import kotlin.experimental.and
-import kotlin.experimental.or
+import org.apache.commons.codec.digest.DigestUtils
 
 /**
  * md5 hash utility
  *
  * @author Mark Vainomaa
  */
-private const val zero_x_ff: Byte = (0xFF).toByte()
-private const val zero_x_hundred: Byte = (0x100).toByte()
 
 internal object Hash {
-    private val md = MessageDigest.getInstance("MD5")
-
-    internal fun md5(text: String): String = StringBuilder().run {
-        md.digest(text.toByteArray()).forEach {
-            append(Integer.toHexString(((it and zero_x_ff) or zero_x_hundred).toInt()).subSequence(1, 3))
-        }
-        toString()
-    }
+    internal fun md5(text: String): String = DigestUtils.md5Hex(text)
 }
